a9016e6add8f97cbff3459dc79c6ae56f1318242
moisesPompilio <moisesjtmed@gmail.com>
2023-03-09 21:57:20 -0300

    estudos de sql

---
+++ comandosIntermediarios.sql
@@ -0,0 +1,56 @@
+SELECT * FROM categoria
+
+SELECT * FROM curso WHERE categoria_id IN(1,2);
+
+SELECT * FROM curso WHERE categoria_id IN(
+    SELECT id FROM categoria WHERE nome NOT LIKE'% %'
+);
+
+SELECT categoria.nome AS categoria,
+        COUNT(curso.id) as numero_cursos
+    FROM categoria
+    JOIN curso ON curso.categoria_id = categoria.id
+GROUP BY categoria;
+
+SELECT categoria,
+       numero_cursos
+    FROM (
+            SELECT categoria.nome AS categoria,
+                COUNT(curso.id) as numero_cursos
+            FROM categoria
+            JOIN curso ON curso.categoria_id = categoria.id
+        GROUP BY categoria
+    ) AS categoria_cursos
+  WHERE numero_cursos > 3;
+
+SELECT categoria,
+       numero_cursos
+    FROM (
+            SELECT categoria.nome AS categoria,
+                COUNT(curso.id) as numero_cursos
+            FROM categoria
+            JOIN curso ON curso.categoria_id = categoria.id
+        GROUP BY categoria
+    ) AS categoria_cursos
+  WHERE numero_cursos >= 3
+  ORDER BY numero_cursos ASC;
+
+    SELECT curso.nome,
+         COUNT(aluno_curso.aluno_id) numero_alunos
+    FROM curso
+    JOIN aluno_curso ON aluno_curso.curso_id = curso.id
+GROUP BY 1
+     HAVING COUNT(aluno_curso.aluno_id) > 2
+ORDER BY numero_alunos DESC;
+
+SELECT (primeiro_nome || ultimo_nome) AS nome_completo,
+    ((NOW()::DATE - data_nascimento) / 365) as idade
+  FROM aluno;
+
+SELECT (primeiro_nome || ultimo_nome) AS nome_completo,
+    AGE(data_nascimento) AS idade
+  FROM aluno;
+
+SELECT (primeiro_nome || ultimo_nome) AS nome_completo,
+    EXTRACT(YEAR FROM AGE(data_nascimento)) AS idade
+  FROM aluno;
--- estudos_sql.session.sql
+++
@@ -1,8 +0,0 @@
-SELECT *
-    FROM aluno
-    JOIN aluno_curso ON aluno_curso.aluno_id = aluno.id;
-   /*  selec os alunos e mostrando as referencias e os cursos*/
-   SELECT *
-    FROM aluno
-    JOIN aluno_curso ON aluno_curso.aluno_id = aluno.id
-    JOIN curso ON curso.id = aluno_curso.curso_id;
